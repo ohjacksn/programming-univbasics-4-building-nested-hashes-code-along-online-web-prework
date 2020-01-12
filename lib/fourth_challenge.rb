98076edd2d2d10117460075faaef00e71a73bd7d
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-12 03:31:06 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -36,8 +36,8 @@ def fourth_challenge
    }
 }
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+#epic_tragedy[:montague][:hero][:status] = "dead"
+#epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
 end
